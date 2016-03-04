78312f2c7272bf672770439a6ad8748b4030e45a
workanator <workanator@gmail.com>
2016-03-04 19:59:31 +0500

    Move items::Path to a separate module items::path::Path

--- src/api/items/mod.rs
+++ src/api/items/mod.rs
@@ -1,3 +1,4 @@
+mod path;
 mod item;
 
 use hyper::method::Method;
@@ -8,61 +9,8 @@ use ::api::MultiOption;
 use ::Result;
 
 
+pub use self::path::Path;
 pub use self::item::{Item, Kind};
-
-
-/// Item path
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub enum Path {
-	/// The Home folder
-	Home,
-	/// The Parent favorite item
-	Favorites,
-	/// The Parent Shared Folders items
-	AllShared,
-	/// The Parent Connectors item
-	Connectors,
-	/// The FileBox item
-	Box,
-	/// The Top item
-	Top,
-	/// The item identified by ID
-	Id(String),
-	/// The absolute path
-	Absolute(String),
-	/// The relative path
-	Relative(String, String),
-	/// Parent
-	Parent(String),
-}
-
-
-impl Path {
-	fn uri(&self, part: Option<&str>, parameters: Option<Parameters>) -> String {
-		let part = match part {
-			Some(part) => part,
-			None => ""
-		};
-
-		let parameters: String = match parameters {
-			Some(opts) => opts.to_string(),
-			None => "".to_owned()
-		};
-
-		match self {
-			&Path::Home => format!("Items(home){}?{}", part, parameters),
-			&Path::Favorites => format!("Items(favorites){}?{}", part, parameters),
-			&Path::AllShared => format!("Items(allshared){}?{}", part, parameters),
-			&Path::Connectors => format!("Items(connectors){}?{}", part, parameters),
-			&Path::Box => format!("Items(box){}?{}", part, parameters),
-			&Path::Top => format!("Items(top){}?{}", part, parameters),
-			&Path::Id(ref id) => format!("Items({}){}?{}", id, part, parameters),
-			&Path::Absolute(ref path) => format!("Items/ByPath?path={}&{}", path, parameters),
-			&Path::Relative(ref id, ref path) => format!("Items({})/ByPath?path={}&{}", id, path, parameters),
-			&Path::Parent(ref id) => format!("Items({})/Parent&{}", id, parameters),
-		}
-	}
-}
 
 
 /// Items Entities
@@ -85,13 +33,13 @@ impl Items {
 	}
 
 	pub fn stat(&self, path: Path, parameters: Option<Parameters>) -> Result<MultiOption<Item>> {
-		self.query(path.uri(None, parameters))
+		self.query(path.entity_and_parameters(None, parameters))
 	}
 
 	pub fn list(&self, path: Path, parameters: Option<Parameters>) -> Result<MultiOption<Item>> {
 		match self.stat(path, None) {
 			Ok(MultiOption::One(item)) => {
-				self.query(Path::Id(item.id).uri(Some("/Children"), parameters))
+				self.query(Path::Id(item.id).entity_and_parameters(Some("/Children"), parameters))
 			},
 			Ok(other) => Ok(other),
 			Err(err) => err.result()
---
+++ src/api/items/path.rs
@@ -0,0 +1,54 @@
+use ::odata::Parameters;
+
+/// Item path
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Path {
+	/// The Home folder
+	Home,
+	/// The Parent favorite item
+	Favorites,
+	/// The Parent Shared Folders items
+	AllShared,
+	/// The Parent Connectors item
+	Connectors,
+	/// The FileBox item
+	Box,
+	/// The Top item
+	Top,
+	/// The item identified by ID
+	Id(String),
+	/// The absolute path
+	Absolute(String),
+	/// The relative path
+	Relative(String, String),
+	/// Parent
+	Parent(String),
+}
+
+
+impl Path {
+	pub fn entity_and_parameters(&self, segment: Option<&str>, parameters: Option<Parameters>) -> String {
+		let segment = match segment {
+			Some(segment) => segment,
+			None => ""
+		};
+
+		let parameters: String = match parameters {
+			Some(opts) => opts.to_string(),
+			None => "".to_owned()
+		};
+
+		match self {
+			&Path::Home => format!("Items(home){}?{}", segment, parameters),
+			&Path::Favorites => format!("Items(favorites){}?{}", segment, parameters),
+			&Path::AllShared => format!("Items(allshared){}?{}", segment, parameters),
+			&Path::Connectors => format!("Items(connectors){}?{}", segment, parameters),
+			&Path::Box => format!("Items(box){}?{}", segment, parameters),
+			&Path::Top => format!("Items(top){}?{}", segment, parameters),
+			&Path::Id(ref id) => format!("Items({}){}?{}", id, segment, parameters),
+			&Path::Absolute(ref path) => format!("Items/ByPath?path={}&{}", path, parameters),
+			&Path::Relative(ref id, ref path) => format!("Items({})/ByPath?path={}&{}", id, path, parameters),
+			&Path::Parent(ref id) => format!("Items({})/Parent&{}", id, parameters),
+		}
+	}
+}
